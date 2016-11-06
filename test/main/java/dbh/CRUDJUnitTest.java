0aac24ddd8d2b01d0eff2599a50b3b90b0748d9c
Paweł Jaruga <helferth95@gmail.com>
2016-11-07 00:51:08 +0100

    Przetestowanie listEmployee()

--- test/main/java/dbh/CRUDJUnitTest.java
+++ test/main/java/dbh/CRUDJUnitTest.java
@@ -5,6 +5,7 @@
  */
 package main.java.dbh;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -22,11 +23,14 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author PM, PJ, SK
+ * @author PM, JPII, SK
  */
 public class CRUDJUnitTest {
     
     private static SessionFactory factory;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    PrintStream nw = new PrintStream(out);
+    PrintStream old = System.out;
     
     public CRUDJUnitTest() {
     }
@@ -41,6 +45,12 @@ public class CRUDJUnitTest {
     
     @Before
     public void setUp() {
+        try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
     }
     
     @After
@@ -57,8 +67,44 @@ public class CRUDJUnitTest {
     @Test
     public void createQueryTest() {
         System.out.println("CRUD Read test: session.createQuery() - for reading purposes");
-        assertTrue(true);
-        //TODO
+        ArrayList set1 = new ArrayList();
+        set1.add(new Certificate("MCA"));
+        
+        ManageEmployee manager = new ManageEmployee(factory);
+        
+        manager.addEmployee("Paweł", "Jaruga", 666000000, set1);
+        
+        System.setOut(nw);
+        manager.listEmployees();
+        System.out.flush();
+        System.setOut(old);
+        
+        //Formatting listEmployees()
+        String result = out.toString();
+        String[] lines = result.split(System.getProperty("line.separator"));
+        CharSequence rem = "Hibernate:";
+        for(int i = 0; i < lines.length; i++)
+        {
+            if(lines[i].contains(rem))
+                lines[i] = "";
+        }
+        StringBuilder res = new StringBuilder();
+        for(String s : lines)
+        {
+            if(!s.equals(""))
+                res.append(s).append(System.getProperty("line.separator"));
+        }
+        result = res.toString();
+        
+        //Building expected string
+        StringBuilder exp = new StringBuilder();
+        exp.append("First Name: Paweł  Last Name: Jaruga  Salary: 666000000").
+            append(System.getProperty("line.separator")).
+            append("Certificate: MCA").
+            append(System.getProperty("line.separator"));
+        String expected = exp.toString();
+        
+        assertEquals(result, expected);
     }
     
     @Test
